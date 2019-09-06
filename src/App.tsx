29ca1e00e88ecdf5211a4a5afc31a922eeefcdbc
Markus Sterner <markus.sterner88@gmail.com>
2019-09-07 00:01:13 +0200

    embed chat window

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <h1>MediBot</h1>
+    <iframe
+      title="MediBot"
+      src="https://webchat.snatchbot.me/2715c805595f52956e1d88c113f1ccce2f6507ca7df65bd4799cf806d6745cd1"
+      style={{ height: '80vh', width: '80vw' }}
+    />
+  </div>
+)
 
-export default App;
+export default App
