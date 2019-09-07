c7d55a67af80a618266b7ae0f27a0dd324f2a9bc
Markus Sterner <markus.sterner88@gmail.com>
2019-09-07 13:37:09 +0200

    black magic snatchbot identification?

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { FirebaseAuthConsumer, FirebaseAuthProvider } from '@react-firebase/auth'
+import firebaseConfig from './firebase-config'
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1 style={{ color: 'red' }}>MediBot</h1>
-    <iframe
-      title="MediBot"
-      src="https://webchat.snatchbot.me/2715c805595f52956e1d88c113f1ccce2f6507ca7df65bd4799cf806d6745cd1"
-      style={{ height: '80vh', width: '80vw' }}
-    />
-  </div>
-)
+firebase.initializeApp(firebaseConfig)
+
+const App: React.FC = () => {
+  useEffect(() => {
+    firebase.auth().signInAnonymously()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1 style={{ color: 'red' }}>MediBot</h1>
+      <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+        <FirebaseAuthConsumer>
+          {({ isSignedIn, user, providerId }) => (
+            <pre style={{ height: 300, overflow: 'auto' }}>
+              {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+            </pre>
+          )}
+        </FirebaseAuthConsumer>
+        <iframe
+          title="MediBot"
+          src="https://webchat.snatchbot.me/2715c805595f52956e1d88c113f1ccce2f6507ca7df65bd4799cf806d6745cd1"
+          style={{ height: '80vh', width: '80vw' }}
+        />
+      </FirebaseAuthProvider>
+    </div>
+  )
+}
 
 export default App
