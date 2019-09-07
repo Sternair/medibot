e164f80889d3109d17508f8aa586b53efd5888b4
Markus Sterner <markus.sterner88@gmail.com>
2019-09-07 20:28:48 +0200

    initial greeting

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const { google } = require("googleapis");
+/* eslint-disable */
+const {google} = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 const functions = require("firebase-functions");
@@ -6,26 +7,26 @@ const functions = require("firebase-functions");
 const googleCredentials = require("./credentials.json");
 
 const ERROR_RESPONSE = {
-  status: "500",
-  message: "There was an error adding an event to your Google calendar"
+    status: "500",
+    message: "There was an error adding an event to your Google calendar"
 };
 const TIME_ZONE = "CET";
 
 
 exports.schedule = functions.https.onRequest((request, response) => {
     console.log(request.body);
-    var timeArray = ["10:00:00","10:40:00","11:20:00","11:35:00","12:00:00","13:00:00"];
-    var dateArray = ["2019-09-09","2019-09-10","2019-09-11"];
+    var timeArray = ["10:00:00", "10:40:00", "11:20:00", "11:35:00", "12:00:00", "13:00:00"];
+    var dateArray = ["2019-09-09", "2019-09-10", "2019-09-11"];
     var name = "James Bond ";
     var type = "MRI "
     var title = "Appointment " + type + name;
     var time = timeArray[Math.floor(Math.random() * timeArray.length)];
     var date = dateArray[Math.floor(Math.random() * dateArray.length)];
     var description = "";
-    var startTime =  date+"T"+time;
-    var endTime =  date+"T"+time;
+    var startTime = date + "T" + time;
+    var endTime = date + "T" + time;
     addEventToCalendar(title, description, startTime, endTime);
-    var msg = "Thank you! Based on your responses, we can schedule your appointment for the "+date+" at "+time+". Is that okay with you?"
+    var msg = "Thank you! Based on your responses, we can schedule your appointment for the " + date + " at " + time + ". Is that okay with you?"
     response.send({
         "message": msg
     });
@@ -33,13 +34,12 @@ exports.schedule = functions.https.onRequest((request, response) => {
 
 exports.patientFind = functions.https.onRequest((request, response) => {
     var msg = request.body.incoming_message;
-    if (msg=="007") {
+    if (msg == "007") {
         response.send({
             "message": "Hello, James Bond! We learned from your General Practitioner that you need a chest MRI. Let's find the best slot for you  & reduce your waiting time for the appointment. Is it OK for you? / Would you like to continue?",
             "suggested_replies": ["Yes", "I'd rather call"]
         });
-    }
-    else {
+    } else {
         response.send({
             "message": "Sorry, could not find this Patient Id, please try again"
         });
@@ -47,68 +47,81 @@ exports.patientFind = functions.https.onRequest((request, response) => {
 
 });
 
-function addEventToCalendar(title, description, startTime, endTime) {
-  /* 
-title: "Title",
-description: "This is a sample description",
-startTime: "2019-09-09T10:00:00",
-endTime: "2019-09-09T13:00:00"
-*/
-  const eventData = {
-    eventName: title,
-    description: description,
-    startTime: startTime,
-    endTime: endTime
-  };
-  const oAuth2Client = new OAuth2(
-    googleCredentials.web.client_id,
-    googleCredentials.web.client_secret,
-    googleCredentials.web.redirect_uris[0]
-  );
+exports.initialGreeting = functions.https.onRequest((request, response) => {
+    const firstNames = ["Mrs. Laura", "Mrs. Susan", "Mrs. Sabrina", "Mrs. Johanna", "Mr. Andreas", "Mr. Michael", "Mr. Sebastian"];
+    const lastNames = ["Jones", "Huber", "Miller", "Mayer", "Untergruber", "Oberhuber"]
+    const treatments = ["chest MRT", "arm X-ray", "stomach ultra sound"]
 
-  oAuth2Client.setCredentials({
-    refresh_token: googleCredentials.refresh_token
-  });
-
-  addEvent(eventData, oAuth2Client)
-    .then(data => {
-      response.status(200).send(data);
-      return;
+    response.send({
+        "message": `Hello ${getRandomElement(firstNames)} ${getRandomElement(lastNames)}! Thank you for uploading your referral slip. We will now guide you through the process of finding an ideal appointment for your ${getRandomElement(treatments)}.`,
+        "suggested_replies": ["Continue", "I want to talk to a person"]
     })
-    .catch(err => {
-      console.error("Error adding event: " + err.message);
-      response.status(500).send(ERROR_RESPONSE);
-      return;
+});
+
+function getRandomElement(array) { return array[Math.floor(Math.random() * array.length)]}
+
+function addEventToCalendar(title, description, startTime, endTime) {
+    /*
+  title: "Title",
+  description: "This is a sample description",
+  startTime: "2019-09-09T10:00:00",
+  endTime: "2019-09-09T13:00:00"
+  */
+    const eventData = {
+        eventName: title,
+        description: description,
+        startTime: startTime,
+        endTime: endTime
+    };
+    const oAuth2Client = new OAuth2(
+        googleCredentials.web.client_id,
+        googleCredentials.web.client_secret,
+        googleCredentials.web.redirect_uris[0]
+    );
+
+    oAuth2Client.setCredentials({
+        refresh_token: googleCredentials.refresh_token
     });
+
+    addEvent(eventData, oAuth2Client)
+        .then(data => {
+            response.status(200).send(data);
+            return;
+        })
+        .catch(err => {
+            console.error("Error adding event: " + err.message);
+            response.status(500).send(ERROR_RESPONSE);
+            return;
+        });
 }
 
 function addEvent(event, auth) {
-  return new Promise(function(resolve, reject) {
-    calendar.events.insert(
-      {
-        auth: auth,
-        calendarId: "primary",
-        resource: {
-          summary: event.eventName,
-          description: event.description,
-          start: {
-            dateTime: event.startTime,
-            timeZone: TIME_ZONE
-          },
-          end: {
-            dateTime: event.endTime,
-            timeZone: TIME_ZONE
-          }
-        }
-      },
-      (err, res) => {
-        if (err) {
-          console.log("Rejecting because of error");
-          reject(err);
-        }
-        console.log("Request successful");
-        resolve(res.data);
-      }
-    );
-  });
+    return new Promise(function (resolve, reject) {
+        calendar.events.insert(
+            {
+                auth: auth,
+                calendarId: "primary",
+                resource: {
+                    summary: event.eventName,
+                    description: event.description,
+                    start: {
+                        dateTime: event.startTime,
+                        timeZone: TIME_ZONE
+                    },
+                    end: {
+                        dateTime: event.endTime,
+                        timeZone: TIME_ZONE
+                    }
+                }
+            },
+            (err, res) => {
+                if (err) {
+                    console.log("Rejecting because of error");
+                    reject(err);
+                }
+                console.log("Request successful");
+                resolve(res.data);
+            }
+        );
+    });
 }
