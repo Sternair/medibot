e4edcc0181ca002275280a3db6854dfae46084bf
Markus Sterner <markus.sterner88@gmail.com>
2019-09-07 14:29:27 +0200

    router

---
+++ src/Patient/Patient.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Patient: React.FC = () => (
+  <div className="App">
+    <h1 style={{ color: 'red' }}>Patient</h1>
+    <iframe
+      title="MediBot"
+      src="https://webchat.snatchbot.me/2715c805595f52956e1d88c113f1ccce2f6507ca7df65bd4799cf806d6745cd1"
+      style={{ height: '80vh', width: '80vw' }}
+    />
+  </div>
+)
+
+export default Patient
--- src/Radiologist/Radiologist.tsx
+++ src/Radiologist/Radiologist.tsx
@@ -1,34 +1,33 @@
-import React from "react";
-import "./Radiologist.css";
-//import ApiCalendar from "react-google-calendar-api/ApiCalendar";
+import React from 'react'
+import './Radiologist.css'
 
 class Radiologist extends React.Component {
   constructor(props: any) {
-    super(props);
+    super(props)
 
     this.state = {
       count: 0
-    };
+    }
 
-    console.log("Constructor!");
+    console.log('Constructor!')
   }
 
   componentDidMount() {
-    console.log("Component has mounted!");
+    console.log('Component has mounted!')
   }
 
   render() {
-    console.log("SimpleCounter render call!");
+    console.log('SimpleCounter render call!')
 
     return (
       <div className="Radiologist">
-        <h1 style={{ color: "red" }}>Radiologist</h1>
+        <h1 style={{ color: 'red' }}>Radiologist</h1>
       </div>
-    );
+    )
   }
 }
 
-export default Radiologist;
+export default Radiologist
 
 // Google API key AIzaSyBSBqHw-J0Q2AJnt3l2KI2fR0mRd-TVtWo
 // Google client ID: 322806710129-4185ksb62m2f50in2dkdoqo3k3hva6do.apps.googleusercontent.com
