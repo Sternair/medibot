b27e0459f2db2f98edb5b8698ebb6a583f4090f7
Manuel Ströhmer <m.stroehmer@gmail.com>
2019-09-07 13:34:26 +0200

    Switched entry component

---
+++ src/Radiologist/Radiologist.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Radiologist.css";
+
+const Radiologist: React.FC = () => (
+  <div className="Radiologist">
+    <h1 style={{ color: "red" }}>Radiologist</h1>
+  </div>
+);
+
+// Google API key AIzaSyBSBqHw-J0Q2AJnt3l2KI2fR0mRd-TVtWo
+
+export default Radiologist;
